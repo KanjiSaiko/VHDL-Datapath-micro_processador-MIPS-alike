8b6252f08f47e08ca46e4655d2c07345fdf84f71
KanjiSaiko <hbortolomiol2@gmail.com>
2024-11-29 02:29:11 -0300

    Separaçao em processos diferentes de cada operaçao e tentativa de fazer um fsm falha :C

--- Datapath _Pipeline/MIPS_Pipeline.vhd
+++ Datapath _Pipeline/MIPS_Pipeline.vhd
@@ -13,8 +13,6 @@ entity MIPS_Pipeline is
 end MIPS_Pipeline;
 
 architecture behavior of MIPS_Pipeline is
-
-    signal PC	: std_logic_vector(7 downto 0); -- Contador de programa (Program Counter) que armazena o endereço atual de execução.
     type mem_dados is array (integer range 0 to 255) of std_logic_vector(7 downto 0);
     type mem_instruc is array (integer range 0 to 255) of std_logic_vector(15 downto 0);
     type banco_regs is array (integer range 0 to 15) of std_logic_vector(7 downto 0);
@@ -43,105 +41,157 @@ architecture behavior of MIPS_Pipeline is
         others => (others => '0') -- Demais posições zeradas
     ); 
 
-    signal regs                 : banco_regs := (others => (others => '0')); --Banco com 16 Registradores
-    signal desvio	            : std_logic; --Controle para indicar se deve ocorrer um salto (branch).
-    signal CalcdesvioID_EX      : std_logic_vector(7 downto 0); --Saída da ULA que executa operações aritméticas.
-    signal ulaEX_MEM, ulaMEM_WB : std_logic_vector(15 downto 0); --Saída da ULA que executa operações aritméticas.
-    signal equal	            : std_logic; --Sinal para verificar se R0 é igual a R1 (usado em instruções de comparação).
-    signal enable_reg_out	    : std_logic; --Habilita a gravação de valores em registradores.
-    signal R0ID_EX, R0EX_MEM    : std_logic_vector(7 downto 0);
-    signal R1ID_EX, R1EX_MEM    : std_logic_vector(7 downto 0);
-    signal PCIF_ID, PCID_EX     : std_logic_vector(7 downto 0); -- Contador de programa (Program Counter) que armazena o endereço atual de execução.
-    signal InIF_ID, InIF_EX, InEX_MEM, InMEM_WB : : std_logic_vector(15 downto 0); --Instrução Atual
-
-
-
-
+    signal PC	                                    : std_logic_vector(7 downto 0); -- Contador de programa (Program Counter)
+    signal regs                                     : banco_regs := (others => (others => '0')); --Banco com 16 Registradores
+    signal desvio	                                : std_logic; --Controle para indicar se deve ocorrer um salto (branch).
+    --signal CalcdesvioID_EX                          : std_logic_vector(7 downto 0); --Saída da ULA que executa operações aritméticas.
+    signal ulaEX_MEM, ulaMEM_WB                     : std_logic_vector(15 downto 0); --Saída da ULA que executa operações aritméticas.
+    signal equal	                                : std_logic; --Sinal para verificar se R0 é igual a R1 (usado em instruções de comparação).
+    signal R0ID_EX, R0EX_MEM                        : std_logic_vector(7 downto 0);
+    signal R1ID_EX, R1EX_MEM                        : std_logic_vector(7 downto 0);
+    signal RwID_EX, RwEX_MEM, RwMEM_WB              : std_logic_vector(7 downto 0); --registrador a ser escrito
+    signal PCIF_ID, PCID_EX, PCEX_MEM, PCMEM_WB     : std_logic_vector(7 downto 0); -- Contador de programa (Program Counter) que armazena o endereço atual de execução.
+    signal InIF_ID, InID_EX, InEX_MEM, InMEM_WB     : std_logic_vector(15 downto 0); --Instrução Atual
 
 begin 
-    
-    R0 <= regs(conv_integer(mem_i(conv_integer(PC))(7 downto 4)));
-
-    R1 <= regs(conv_integer(mem_i(conv_integer(PC))(3 downto 0)));
-    R1i <= (("0000") & mem_i(conv_integer(PC))(3 downto 0));
-
             --Verifica se R0 e R1 têm valores iguais.
-            equal <= '1' when (R0 = R1) else
+            equal <= '1' when (R0EX_MEM = RwEX_MEM) else
                 '0';
 
             --Indica se um salto deve ocorrer. Fazer Bolhas
-            desvio <= '1' when (mem_i(conv_integer(PC))(15 downto 12) = "0100" and equal = '0') or (mem_i(conv_integer(PC))(15 downto 12) = "0101" and equal = '1') or (mem_i(conv_integer(PC))(15 downto 12) = "0110" and equal = '0') else
+            desvio <= '1' when (InEX_MEM(15 downto 12) = "0110" and equal = '0') or (InEX_MEM(15 downto 12) = "0101" and equal = '1') else
                 '0';
-    
-    ula <= R0 * R1;
-    ulai <= R0 * R1i;
 
-    R0_out <= R0;
-    R1_out <= R1;
+            R0_out <= R0EX_MEM;
+            R1_out <= R1EX_MEM;
 
     process(reset, clock)
         begin
             if (reset = '1') then   --Se reset está ativo (1), ele zera tudo
                 regs    <= (others => (others => '0'));
                 PC      <= (others => '0');
+                PCIF_ID <= (others => '0');
+                PCID_EX <= (others => '0');
+                PCEX_MEM <= (others => '0');
+                PCMEM_WB <= (others => '0');
+
+                InIF_ID   <= (others => '0');
+                InID_EX   <= (others => '0');
+                InEX_MEM  <= (others => '0');
+                InMEM_WB  <= (others => '0');
+
+                ulaEX_MEM <= (others => '0');
+                ulaMEM_WB <= (others => '0');
+
+                R0ID_EX   <= (others => '0');
+                R1ID_EX   <= (others => '0');
+                R0EX_MEM  <= (others => '0');
+                R1EX_MEM  <= (others => '0');
+                RwID_EX  <= (others => '0');
+                RwEX_MEM  <= (others => '0');
+                RwMEM_WB  <= (others => '0');
 
             elsif (clock = '1' and clock'event) then
+                InIF_ID <= mem_i(conv_integer(PC))(15 downto 0);
+                InID_EX <= InIF_ID;
+                InEX_MEM <= InID_EX;
+                InMEM_WB <= InEX_MEM;
 
-                
+                PCIF_ID <= PC;
+                PCID_EX <= PCIF_ID;
 
+                if (desvio = '1') then
 
+                    if (InMEM_WB(15 downto 12) = "0101" or InMEM_WB(15 downto 12) = "0110") then
+                        PC <= PC + InMEM_WB(3 downto 0); -- BEQ/BNE
 
-
-
-
-
-                -- Decodificação e execução
-                case mem_i(conv_integer(PC))(15 downto 12) is
-                    when "0000" => -- LOAD
-                        regs(conv_integer(mem_i(conv_integer(PC))(11 downto 8))) <= mem_d(conv_integer(mem_i(conv_integer(PC))(7 downto 0)));
-                    when "1010" => -- LOAD-I
-                        regs(conv_integer(mem_i(conv_integer(PC))(11 downto 8))) <= mem_i(conv_integer(PC))(7 downto 0);
-                    
-                    when "0111" => -- STORE
-                        mem_d(conv_integer(mem_i(conv_integer(PC))(7 downto 0))) <= regs(conv_integer(mem_i(conv_integer(PC))(11 downto 8)));
-                    
-                    when "0001" => -- ADD
-                        regs(conv_integer(mem_i(conv_integer(PC))(11 downto 8))) <= R0 + R1;
-
-                    when "0010" => -- SUB
-                        regs(conv_integer(mem_i(conv_integer(PC))(11 downto 8))) <= R0 - R1;
-                    
-                    when "1011" => -- SUI
-                        regs(conv_integer(mem_i(conv_integer(PC))(11 downto 8))) <= R0 - mem_i(conv_integer(PC))(3 downto 0);
-
-                    when "0011" => -- MULT
-                        regs(conv_integer(mem_i(conv_integer(PC))(11 downto 8))) <= ula(7 downto 0);
-                
-                    when "1000" => -- MUI
-                        regs(conv_integer(mem_i(conv_integer(PC))(11 downto 8))) <= ulai(7 downto 0);
-
-                    when "1001" => -- ADDI
-                        regs(conv_integer(mem_i(conv_integer(PC))(11 downto 8))) <= R0 + mem_i(conv_integer(PC))(3 downto 0);
-
-                    when others =>
-                        
-                    end case;
-
-                if (desvio = '0') then --Se não houver desvio.
-                    PC <= PC + 1;
-
-                else --Caso contrário, ele pula para o endereço indicado. --JUMP, BEQ OU BQE
-                    if (mem_i(conv_integer(PC))(15 downto 12) = "0110" or mem_i(conv_integer(PC))(15 downto 12) = "0101") then
-                        
-                        PC <= PC + mem_i(conv_integer(PC))(2 downto 0);
-                    
-                    elsif (mem_i(conv_integer(PC))(15 downto 12) = "0100") then
-                        
-                        PC <= mem_i(conv_integer(PC))(7 downto 0);
+                    elsif (InMEM_WB(15 downto 12) = "0100") then
+                        PC <= InMEM_WB(7 downto 0); -- JMP
 
                     end if;
+
+                else
+                    PC <= PC + 1;
+                    
                 end if;
             end if;
     end process;
 
+    --ID_EX
+    process(InID_EX)
+        begin
+            if(InID_EX(15 downto 12) = "0100") then --jump
+
+            elsif(InID_EX(15 downto 12) = "0001" or InID_EX(15 downto 12) = "0010" or InID_EX(15 downto 12) = "0011") then --R
+                R0ID_EX <= regs(conv_integer(InID_EX(7 downto 4)));
+                R1ID_EX <= regs(conv_integer(InID_EX(3 downto 0)));
+                RwID_EX <= regs(conv_integer(InID_EX(11 downto 8)));
+
+            else --Imediatos e BEQ/BNE
+                R0ID_EX <= regs(conv_integer(InID_EX(7 downto 4)));
+                RwID_EX <= regs(conv_integer(InID_EX(11 downto 8)));
+            
+            end if;
+    end process;
+
+    
+    --EX_MEM
+    process(InEX_MEM)
+        variable temp_PC : std_logic_vector(7 downto 0);
+        begin
+            R0EX_MEM <= R0ID_EX;
+            R1EX_MEM <= R1ID_EX;
+            RwEX_MEM <= RwID_EX;
+            temp_PC := PCID_EX;
+
+            if(desvio = '1') then
+                temp_PC := temp_PC + (("0000") & InEX_MEM(3 downto 0));
+            
+            else
+                case InEX_MEM(15 downto 12) is
+                    when "0001" => -- ADD
+                        ulaEX_MEM <= (("00000000") & (R0EX_MEM + R1EX_MEM));
+
+                    when "0010" => -- SUB
+                        ulaEX_MEM <= (("00000000") & (R0EX_MEM - R1EX_MEM));
+
+                    when "0011" => -- MULT
+                        ulaEX_MEM <= R0EX_MEM * R1EX_MEM;
+                        
+                    when "1001" => -- ADDI
+                        ulaEX_MEM <= (("00000000") & (R0EX_MEM + (("0000") & InEX_MEM(3 downto 0))));
+
+                    when "1011" => -- SUI
+                        ulaEX_MEM <= (("00000000") & (R0EX_MEM - (("0000") & InEX_MEM(3 downto 0))));    
+                
+                    when "1000" => -- MUI
+                        ulaEX_MEM <= R0EX_MEM * (("0000") & InEX_MEM(3 downto 0));
+
+                    when others =>
+                        
+                    end case;
+            end if;
+            PCEX_MEM <= temp_PC;
+    end process;
+
+    process(InMEM_WB)
+        begin
+            PCMEM_WB <= PCEX_MEM;
+            RwMEM_WB <= RwEX_MEM;
+            ulaMEM_WB <= ulaEX_MEM;
+
+            if(InMEM_WB(15 downto 12) = "0111") then --STORE
+                mem_d(conv_integer(InMEM_WB(7 downto 0))) <= RwMEM_WB;
+            
+            elsif(InMEM_WB(15 downto 12) = "0000") then -- LOAD
+                regs(conv_integer(InMEM_WB(15 downto 12))) <= mem_d(conv_integer(InMEM_WB(7 downto 0)));
+
+            elsif(InMEM_WB(15 downto 12) = "0000") then -- LOAD-I
+                regs(conv_integer(InMEM_WB(15 downto 12))) <= InMEM_WB(7 downto 0);
+            
+            else --TIPO R
+                regs(conv_integer(InMEM_WB(15 downto 12))) <= ulaMEM_WB(7 downto 0);
+
+            end if;
+        end process;
 end behavior;
